432b8b6efceae4986c7d4674c167a92d63037fec
Mugisho-dev-metasploit <mugishomerci445@gmail.com>
2026-03-30 17:58:38 +0200

    [SETUP] Tailwind CSS configuration and base structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Primary
+        red: {
+          400: 'hsl(7, 99%, 70%)',
+        },
+        yellow: {
+          500: 'hsl(51, 100%, 49%)',
+        },
+        'green-dark': {
+          800: 'hsl(167, 40%, 24%)',
+        },
+        'blue-dark': {
+          800: 'hsl(198, 62%, 26%)',
+        },
+        'green-medium': {
+          500: 'hsl(167, 34%, 41%)',
+        },
+        // Neutral
+        'grey-dark': {
+          950: 'hsl(212, 27%, 19%)',
+        },
+        'grey-semi': {
+          600: 'hsl(213, 9%, 39%)',
+          550: 'hsl(232, 10%, 55%)',
+          400: 'hsl(210, 4%, 67%)',
+        },
+        white: 'hsl(0, 100%, 100%)',
+      },
+      fontFamily: {
+        barlow: ['Barlow', 'sans-serif'],
+        fraunces: ['Fraunces', 'serif'],
+      },
+      fontSize: {
+        body: '18px',
+      },
+    },
   },
   plugins: [],
 }
-
